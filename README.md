e829ace6ee62c9228cce7dddff10981f57bb21cd
Syahiid Nur Kamil <syahiid.kamil@mclinica.com>
2022-03-04 23:30:14 +0700

    test(first): fix unit tests

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ type
 `chore` – changes that do not relate to a fix or feature and don't modify src or test files (for example updating dependencies)
 `refactor` – refactored code that neither fixes a bug nor adds a feature
 `docs` – updates to documentation such as a the README or other markdown files
-`docs` – including new or correcting previous tests
+`test` – including new or correcting previous tests
 `perf` – performance improvements
 `ci` – continuous integration related
 `build` – changes that affect the build system or external dependencies
--- src/books/controllers/books.controller.spec.ts
+++ src/books/controllers/books.controller.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { BookEntity } from '../entities/book.entity';
+import { BooksService } from '../services/books.service';
 import { BooksController } from './books.controller';
 
 describe('BooksController', () => {
@@ -7,6 +11,18 @@ describe('BooksController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BooksController],
+      providers: [BooksService,
+        {
+          provide: BooksService,
+          useValue: {
+            getBooks: jest.fn(),
+            findOne: jest.fn(),
+            createBook: jest.fn(),
+            putBook: jest.fn(),
+            deleteBook: jest.fn()
+          }
+        }
+      ],
     }).compile();
 
     controller = module.get<BooksController>(BooksController);
