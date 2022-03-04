e4f430c967e8e6b82ba189411a94109692f903c8
Syahiid Nur Kamil <syahiid.kamil@mclinica.com>
2022-03-04 20:03:27 +0700

    feat(first): configure orm, detail data type, and structuring app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
