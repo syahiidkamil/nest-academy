b1d8cd037ea5061978722f885eebdb5239c8aea6
Syahiid Nur Kamil <syahiid.kamil@mclinica.com>
2022-03-13 17:12:30 +0700

    feat(third): throw the details of error messages(its property and its error)

--- src/common/pipes/validation.pipe.ts
+++ src/common/pipes/validation.pipe.ts
@@ -4,7 +4,7 @@ import {
   ArgumentMetadata,
   BadRequestException,
 } from '@nestjs/common';
-import { validate } from 'class-validator';
+import { validate, ValidationError } from 'class-validator';
 import { plainToClass } from 'class-transformer';
 
 @Injectable()
@@ -15,16 +15,39 @@ export class ValidationPipe implements PipeTransform<any> {
     }
     const object = plainToClass(metatype, value);
     const errors = await validate(object);
-    if (errors.length > 0) {
-      console.log(errors);
-
-      throw new BadRequestException('Validation failed');
+    const { length: errorsLength } = errors;
+    if (errorsLength > 0) {
+      this.handleErrors(errors);
     }
     return value;
+  }
+
+  private handleErrors(errors: ValidationError[]){
+    const { length: errorsLength } = errors;
+    if (errorsLength > 0) {
+      const errorMessages = errors.reduce(
+        (stringAccumulator, errorObject, index) => {
+          const key = Object.keys(errorObject.constraints)[0];
+          const errorMessageArray = errorObject.constraints[key].split(' ');
+          const firstWord = errorMessageArray[0];
+          errorMessageArray[0] = this.camelToSnakeCase(firstWord);
+          const prefix = index === 0 ? '' : ' ';
+          const suffix = index === (errorsLength - 1) ? '.': ',';
+          return stringAccumulator += `${prefix}${errorMessageArray.join(' ')}${suffix}`;
+        },
+        ''
+      );
+
+      throw new BadRequestException(errorMessages);
+    }
   }
 
   private toValidate(metatype: Function): boolean {
     const types: Function[] = [String, Boolean, Number, Array, Object];
     return !types.includes(metatype);
+  }
+
+  private camelToSnakeCase(str){
+    return str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
   }
 }
