879beea8aea7e9f802af2cb39125a144ce431195
Syahiid Nur Kamil <syahiid.kamil@mclinica.com>
2022-03-13 10:54:01 +0700

    refactor(second): get configurations from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './common/config/configuration';
+import databaseConfig from './common/config/database.config';
 
 @Module({
-  imports: [BooksModule, TypeOrmModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration, databaseConfig],
+      isGlobal: true,
+    }),
+    BooksModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const options = configService.get('database');
+
+        return options;
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import { NestApplicationOptions, VersioningType } from '@nestjs/common';
+import { VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('port');
   app.enableVersioning({
     type: VersioningType.URI
   });
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
